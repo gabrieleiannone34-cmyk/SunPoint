2642d24637dcd2c0b5469bd2c2c46bc11257049a
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-06 17:44:38 +0200

    "Dichiarazioni variabili classe Utente.java"

--- src/main/java/it/unisa/sunpoint/control/Utente.java
+++ src/main/java/it/unisa/sunpoint/control/Utente.java
@@ -3,6 +3,15 @@ package it.unisa.sunpoint.control;
 import java.io.Serializable;
 
 public class Utente implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
+	private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String passwordHash; 
+    private String indirizzo;
+    private String citta;
+    private String role; // user o admin (default admin)
 	
 }
