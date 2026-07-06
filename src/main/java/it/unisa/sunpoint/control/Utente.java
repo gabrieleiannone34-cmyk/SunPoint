192fc159d8a799f9dc8f78a6a3a48889c00ed310
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-06 18:00:54 +0200

    "inserimento classe costruttore e metodi get e set"

--- src/main/java/it/unisa/sunpoint/control/Utente.java
+++ src/main/java/it/unisa/sunpoint/control/Utente.java
@@ -14,4 +14,64 @@ public class Utente implements Serializable {
     private String citta;
     private String role; // user o admin (default admin)
 	
+    public Utente() {
+    }
+    
+    public int getId() { 
+    	return id; 
+    	}
+    public void setId(int id) { 
+    	this.id = id; 
+    	}
+    
+    
+    public String getNome() { 
+    	return nome; 
+    	}
+    public void setNome(String nome) { 
+    	this.nome = nome; 
+    	}
+
+    public String getCognome() { 
+    	return cognome; 
+    	}
+    public void setCognome(String cognome) { 
+    	this.cognome = cognome; 
+    	}
+
+    public String getEmail() { 
+    	return email; 
+    	}
+    public void setEmail(String email) { 
+    	this.email = email; 
+    	}
+
+    public String getPasswordHash() { 
+    	return passwordHash; 
+    	}
+    public void setPasswordHash(String passwordHash) { 
+    	this.passwordHash = passwordHash; 
+    	}
+
+    public String getIndirizzo() { 
+    	return indirizzo; 
+    	}
+    public void setIndirizzo(String indirizzo) { 
+    	this.indirizzo = indirizzo; 
+    	}
+
+    public String getCitta() { 
+    	return citta; 
+    	}
+    public void setCitta(String citta) { 
+    	this.citta = citta; 
+    	}
+
+    public String getRole() { 
+    	return role; 
+    	}
+    public void setRole(String role) { 
+    	this.role = role; 
+    	}
 }
+
