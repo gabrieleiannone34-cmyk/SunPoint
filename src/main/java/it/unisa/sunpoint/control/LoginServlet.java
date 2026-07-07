89c5765f69709e75414b338d2b11233f7470c02f
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-07 17:00:05 +0200

    "Modifica del file LoginServlet.java"

--- src/main/java/it/unisa/sunpoint/control/LoginServlet.java
+++ src/main/java/it/unisa/sunpoint/control/LoginServlet.java
@@ -1,12 +1,20 @@
 package it.unisa.sunpoint.control;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
+import it.unisa.sunpoint.model.Utente;
+import it.unisa.sunpoint.dao.UtenteDAO;
 /**
  * Servlet implementation class LoginServlet
  */
@@ -14,28 +22,42 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// Mostra la pagina di login (nascosta nella cartella WEB-INF)
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
+        dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// Riceve i dati dal form ed effettua il controllo
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		UtenteDAO utenteDAO = new UtenteDAO();
+		
+	try {
+		Utente utente = utenteDAO.doRetrieveByEmailAndPassword(email, password);
+		
+		if(utente != null) {
+			//Login riuscito, creiamo la sessione HTTP e salviamo l'intero oggetto Utente
+			HttpSession session = request.getSession();
+			session.setAttribute("utenteLoggato", utente);
+			
+			// Reindirizziamo l'utente alla home page
+			response.sendRedirect(request.getContextPath() + "/index.jsp");
+		} else {
+			// Login fallito
+			request.setAttribute("errore", "Email o password non validi. Riprova.");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
+			dispatcher.forward(request, response);
+		}
+	} catch(SQLException e) {
+		e.printStackTrace();
+		request.setAttribute("errore", "Errore di connessione al database.");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
+        dispatcher.forward(request, response);
+			
+		}
 	}
 
 }
