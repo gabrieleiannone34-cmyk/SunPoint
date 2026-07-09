a4a9c7c1c9b6eba6c1354d297c192e18d9a27f50
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-09 14:35:56 +0200

    "Connessione al pool"

--- src/main/java/it/unisa/sunpoint/dao/ProdottoDAO.java
+++ src/main/java/it/unisa/sunpoint/dao/ProdottoDAO.java
@@ -1,5 +1,25 @@
 package it.unisa.sunpoint.dao;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
 public class ProdottoDAO {
 
+	private static DataSource ds;
+	
+	static {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			ds = (DataSource) envCtx.lookup("jdbc/sunpoint");
+		} catch (NamingException e) {
+			System.out.println("Errore JNDI: " + e.getMessage());
+		}
+	}
+	
+	private static final String TABLE_NAME = "Prodotti";
+	
 }
