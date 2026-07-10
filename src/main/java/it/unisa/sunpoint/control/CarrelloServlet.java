9a0e23a34794bae81c6b977ef7f7a1123bb68717
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-10 13:30:03 +0200

    "File CarrelloServlet.java creato"

---
+++ src/main/java/it/unisa/sunpoint/control/CarrelloServlet.java
@@ -0,0 +1,29 @@
+package it.unisa.sunpoint.control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class CarrelloServlet
+ */
+@WebServlet("/CarrelloServlet")
+public class CarrelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
