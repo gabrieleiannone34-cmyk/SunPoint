8172808f6d3e3fa4f770c048336eea6f68abe411
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-10 11:45:32 +0200

    "Spostiamo CatalogoServlet nella cartella giusta .control"

--- src/main/java/it/unisa/sunpoint/control/CatalogoServlet.java
+++ src/main/java/it/unisa/sunpoint/control/CatalogoServlet.java
@@ -1,4 +1,4 @@
-package it.unisa.sunpoint.dao;
+package it.unisa.sunpoint.control;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -7,32 +7,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Servlet implementation class CatalogoServlet
- */
-@WebServlet("/CatalogoServlet")
+
 public class CatalogoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CatalogoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	// Usiamo il doGet perché l'utente sta solo richiedendo di VEDERE una pagina, non sta inviando un form
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ProdottoDAO prodottoDAO = new ProdottoDAO();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
