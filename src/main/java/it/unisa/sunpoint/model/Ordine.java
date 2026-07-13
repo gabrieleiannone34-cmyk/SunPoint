1a571b2794f5d3180197bc61517b35c46a1c0ba7
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-13 14:01:21 +0200

    "Dichiarazione e definizione variabili costruttore e metodi get e set"

--- src/main/java/it/unisa/sunpoint/model/Ordine.java
+++ src/main/java/it/unisa/sunpoint/model/Ordine.java
@@ -1,5 +1,47 @@
 package it.unisa.sunpoint.model;
 
-public class Ordine {
+import java.sql.Timestamp;
 
+public class Ordine {
+	private int id;
+	private int userId;
+	private double totale;
+	private Timestamp dataOrdine;
+	
+	public Ordine() {
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public double getTotale() {
+		return totale;
+	}
+
+	public void setTotale(double totale) {
+		this.totale = totale;
+	}
+
+	public Timestamp getDataOrdine() {
+		return dataOrdine;
+	}
+
+	public void setDataOrdine(Timestamp dataOrdine) {
+		this.dataOrdine = dataOrdine;
+	}
+	
+	
 }
