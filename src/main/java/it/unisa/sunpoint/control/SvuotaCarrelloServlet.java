d2a9d06b8db8af4d538f8ed63b946629b371590d
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-13 12:35:00 +0200

    "Definizione metodo doGet"

--- src/main/java/it/unisa/sunpoint/control/SvuotaCarrelloServlet.java
+++ src/main/java/it/unisa/sunpoint/control/SvuotaCarrelloServlet.java
@@ -5,34 +5,27 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
-/**
- * Servlet implementation class SvuotaCarrelloServlet
- */
-@WebServlet("/SvuotaCarrelloServlet")
 public class SvuotaCarrelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SvuotaCarrelloServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		// Recuperiamo la sessione dell'utente
+		HttpSession session = request.getSession();
+		
+		// Rimuoviamo SOLO l'attributo "carrello".
+		// L'utente rimane loggato 
+		session.removeAttribute("carrello");
+		
+		//Rimandiamo l'utente alla pagina del carello (che ora risulterà vuoto)
+		response.sendRedirect(request.getContextPath() + "/carrello.jsp");
+}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
