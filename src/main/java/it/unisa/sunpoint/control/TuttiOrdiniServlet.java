d92b6305f505dd6a4b132786c80cdfd7620f97b3
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-15 15:28:52 +0200

    "Definizione metodo doGet"

--- src/main/java/it/unisa/sunpoint/control/TuttiOrdiniServlet.java
+++ src/main/java/it/unisa/sunpoint/control/TuttiOrdiniServlet.java
@@ -1,40 +1,57 @@
 package it.unisa.sunpoint.control;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class TuttiOrdiniServlet
- */
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import it.unisa.sunpoint.dao.OrdineDAO;
+import it.unisa.sunpoint.model.Ordine;
+import it.unisa.sunpoint.model.Utente;
+
+
 @WebServlet("/TuttiOrdiniServlet")
 public class TuttiOrdiniServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TuttiOrdiniServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		Utente utente = (Utente) session.getAttribute("utenteLoggato");
+		
+		//Solo gli admin possono vedere questa pagina
+		if (utente == null || !"admin".equals(utente.getRole())) {
+			response.sendRedirect(request.getContextPath() + "/index.jsp");
+			return;
+		}
+		
+		OrdineDAO ordineDAO = new OrdineDAO();
+		try {
+			// L'Admin chiede tutti gli ordini del negozio
+			List<Ordine> tuttiOrdini = ordineDAO.doRetrieveAll();
+			
+			// Li mettiamo nello "zaino"
+			request.setAttribute("tuttiOrdini", tuttiOrdini);
+		
+			// Apriamo la porta della pagina riservata
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/tuttiOrdini.jsp");
+			dispatcher.forward(request, response);
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+			response.getWriter().println("Errore durante il recupero degli ordini.");
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
