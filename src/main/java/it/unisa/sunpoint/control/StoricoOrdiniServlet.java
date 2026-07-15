4e662445604340f3084bda5002e4e351e9a12286
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-15 10:38:38 +0200

    "Definizione metodo doGet"

--- src/main/java/it/unisa/sunpoint/control/StoricoOrdiniServlet.java
+++ src/main/java/it/unisa/sunpoint/control/StoricoOrdiniServlet.java
@@ -1,40 +1,58 @@
 package it.unisa.sunpoint.control;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class StoricoOrdiniServlet
- */
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import it.unisa.sunpoint.dao.OrdineDAO;
+import it.unisa.sunpoint.model.Ordine;
+import it.unisa.sunpoint.model.Utente;
+
+
 @WebServlet("/StoricoOrdiniServlet")
 public class StoricoOrdiniServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public StoricoOrdiniServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		Utente utente = (Utente) session.getAttribute("utenteLoggato");
+		
+		//Verifica se l'utente è loggato(se non è loggatto va alla pagina di login)
+		if(utente == null) {
+			response.sendRedirect(request.getContextPath() + "/LoginServlet");
+			return;
+		}
+		OrdineDAO ordineDAO = new OrdineDAO();
+		try {
+			// 2. Chiediamo al database tutti gli ordini di questo utente
+			List<Ordine> storicoOrdini = ordineDAO.doRetrieveByUserId(utente.getId());
+			
+			// 3. Inseriamo la lista nella request
+			// (Usiamo la request e non la sessione perché è un dato "usa e getta" per visualizzare la pagina)
+			request.setAttribute("storicoOrdini", storicoOrdini);
+			
+			// 4. Apriamo la porta della JSP nascosta
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/ordini.jsp");
+			dispatcher.forward(request, response);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			response.getWriter().println("Errore durante il recupero dello storico ordini.");
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
 		doGet(request, response);
 	}
 
