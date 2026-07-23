4559e4bf8f55c1eb370f9d98e36dad545590efb7
gabrieleiannone34-cmyk <gabrieleiannone34@gmail.com>
2026-07-23 11:28:59 +0200

    "definizione classe ItemCarrello"

--- src/main/java/it/unisa/sunpoint/model/ItemCarrello.java
+++ src/main/java/it/unisa/sunpoint/model/ItemCarrello.java
@@ -1,5 +1,30 @@
 package it.unisa.sunpoint.model;
 
 public class ItemCarrello {
+	private Prodotto prodotto;
+    private int quantita;
+	
+    public ItemCarrello(Prodotto prodotto, int quantita) {
+		super();
+		this.prodotto = prodotto;
+		this.quantita = quantita;
+	}
 
+	public Prodotto getProdotto() {
+		return prodotto;
+	}
+
+	public void setProdotto(Prodotto prodotto) {
+		this.prodotto = prodotto;
+	}
+
+	public int getQuantita() {
+		return quantita;
+	}
+
+	public void setQuantita(int quantita) {
+		this.quantita = quantita;
+	}
+    
+    
 }
